9d3615a4ff8f9494d29b52e87682c876d9b6838b
Sackitey68 <sackitey68@gmail.com>
2023-12-14 16:07:03 +0000

    Added functions to saved notes in JSON

--- main.js
+++ main.js
@@ -8,10 +8,37 @@ function addNote(){
     note.classList.add("note");
     note.innerHTML = `
         <div class="toolbar">
-            <i class="fas fa-save"></i>
-            <i class="fas fa-trash"></i>
+            <i class=" save fas fa-save"></i>
+            <i class=" trash fas fa-trash"></i>
         </div>
             <textarea name="" id="" cols="30" rows="10"></textarea> 
     `;
+
+
+    const save = note.querySelector(".save");
+    const trash = note.querySelector(".trash");
+    const textarea = note.querySelector("textarea");
+
+    save.addEventListener("click", saveNotes);
+    textarea.addEventListener("input", saveNotes)
+    trash.addEventListener("click", () => {
+        note.remove();
+        saveNotes();
+
+    })
+
      main.appendChild(note);
+}
+
+function saveNotes() {
+    const notes = document.querySelectorAll(".note textarea");
+    const data = Array.from(notes).map(note => note.value);
+    console.log(notes, data);
+
+    if (data.length === 0){
+        localStorage.removeItem("notes");
+    }
+    else{
+        localStorage.setItem("notes", JSON.stringify(data))
+    }
 }
