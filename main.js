bc02afd2440b749d5d66bc9806df3a3971687c88
Sackitey68 <sackitey68@gmail.com>
2023-12-14 17:13:56 +0000

    Readme updated

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 const addBtn = document.querySelector("#addBtn");
 const main = document.querySelector("#main");
 
+
+// This function listens to click event when the user clicks add Note
 addBtn.addEventListener("click", addNote);
 
+
+// When the user clicks on add Note this function pop up the textarea
 function addNote(){
     const note  = document.createElement("div");
     note.classList.add("note");
@@ -19,6 +23,8 @@ function addNote(){
     const trash = note.querySelector(".trash");
     const textarea = note.querySelector("textarea");
 
+
+    // This function listens to saves the notes when the user clicks the save icon
     save.addEventListener("click", saveNotes);
     textarea.addEventListener("input", saveNotes)
     trash.addEventListener("click", () => {
@@ -30,6 +36,7 @@ function addNote(){
      main.appendChild(note);
 }
 
+// This functions saves the last note after being refreshed
 function saveNotes() {
     const notes = document.querySelectorAll(".note textarea");
     const data = Array.from(notes).map(note => note.value);
@@ -43,6 +50,7 @@ function saveNotes() {
     }
 }
 
+// Saves the function in JSON 
 function loadNotes(){
     const isNotes = JSON.parse(localStorage.getItem("notes"));
 
